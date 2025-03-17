7f8ab428826c7973f6086a113d7a10959ab32c90
Noel Chiasson <noel@discoverygarden.ca>
2025-03-17 14:07:48 -0300

    Basic test of the OAI PMH record generation and endpoint loading

---
+++ playwright/tests/oai.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+import * as path from 'path';
+
+test.use({
+    ignoreHTTPSErrors: true
+});
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('user');
+    await page.getByLabel('Username').click();
+    await page.getByLabel('Username').fill('islandora');
+    await page.getByLabel('Password').click();
+    await page.getByLabel('Password').fill('islandora');
+    await page.getByRole('button', { name: 'Log in' }).click();
+});
+
+test('OAI-PMH Records and Endpoint', async ({ page }) => {
+    await page.goto('admin/config/services/rest/oai-pmh/queue');
+    await page.getByRole('button', { name: 'Rebuild OAI-PMH' }).click();
+    await page.waitForURL('**/admin/config/services/rest/oai-pmh/queue');
+
+    // Check to see if OAI-PMH Record can be succesfully rebuilt.
+    await expect(page.getByLabel('Status message').getByText('Successfully rebuilt your OAI')).toBeVisible();
+
+    await page.goto('oai/request?verb=ListRecords&metadataPrefix=oai_dc');
+
+    const response = await page.request.get('oai/request?verb=ListRecords&metadataPrefix=oai_dc');
+    await expect(response).toBeOK();
+});
